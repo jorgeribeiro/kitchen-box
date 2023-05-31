26c32f6eb9af8ac9ce5fa6e27cd7373185f98220
Jorge Ribeiro <joorgemelo@gmail.com>
2023-05-31 20:54:28 +0400

    Include id in resources

--- app/Http/Resources/IngredientResource.php
+++ app/Http/Resources/IngredientResource.php
@@ -15,6 +15,7 @@ class IngredientResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'measure' => $this->measure,
             'supplier' => $this->supplier,
