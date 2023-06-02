d622ceebb0ddb46d668d861eaed8804d3b315461
Jorge Ribeiro <joorgemelo@gmail.com>
2023-06-02 09:27:59 +0400

    Update app initialization

--- start.sh
+++ start.sh
@@ -5,17 +5,6 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-# Check if APP_KEY is already set
-if grep -q "^APP_KEY=" .env; then
-    echo "APP_KEY is already set in the .env file."
-else
-    # Generate app key if it doesn't exist
-    docker-compose exec app php artisan key:generate --ansi
-fi
-
-# Install Composer dependencies
-docker-compose exec app composer install --no-interaction --prefer-dist
-
 # Start the Docker containers
 docker-compose up -d
 
@@ -24,6 +13,24 @@ echo "Waiting for the app container to be ready..."
 until docker-compose exec app php -v >/dev/null 2>&1; do
     sleep 1
 done
+
+# Install Composer dependencies
+docker-compose exec app composer install --no-interaction --prefer-dist
+
+# Check if APP_KEY is already set
+if grep -q "^APP_KEY=.*" .env; then
+    existing_key=$(grep "^APP_KEY=.*" .env)
+    if [[ "$existing_key" == "APP_KEY=" ]]; then
+        docker-compose exec app php artisan key:generate --ansi
+        echo "Generated a new APP_KEY in the .env file."
+    else
+        echo "APP_KEY is already set in the .env file."
+    fi
+else
+    # Generate app key if it doesn't exist
+    docker-compose exec app php artisan key:generate --ansi
+    echo "Generated a new APP_KEY in the .env file."
+fi
 
 # Run database migrations
 docker-compose exec app php artisan migrate
